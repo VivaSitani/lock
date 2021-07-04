cd16ff38f63d757d6d30d97f1e3b0c78a727f35d
VivaSitani <66580922+VivaSitani@users.noreply.github.com>
2021-07-04 15:29:55 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,26 +2,27 @@ input.onButtonPressed(Button.A, function () {
     temp = "" + user_key + "A"
     user_key = temp
     serial.writeLine("" + (user_key))
-    if (user_key == key) {
-        pins.digitalWritePin(DigitalPin.P0, 1)
-    }
 })
 input.onButtonPressed(Button.B, function () {
     temp = "" + user_key + "B"
     user_key = temp
     serial.writeLine("" + (user_key))
-    if (user_key == key) {
-        pins.digitalWritePin(DigitalPin.P0, 1)
-        basic.pause(100)
-        pins.servoWritePin(AnalogPin.P1, 0)
-    }
 })
 let temp = ""
 let user_key = ""
-let key = ""
 pins.servoWritePin(AnalogPin.P1, 180)
-key = "ABBABAAABA"
+pins.digitalWritePin(DigitalPin.P12, 1)
+let key = "ABBABAAABA"
 user_key = ""
+basic.forever(function () {
+    if (user_key == key) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.servoWritePin(AnalogPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P12, 0)
+    } else {
+        pins.digitalWritePin(DigitalPin.P12, 1)
+    }
+})
 basic.forever(function () {
 	
 })
